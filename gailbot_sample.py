d4a616d17da561763d708f8ef60438ea875898a8
Yike Li Viv <86906727+YikeLi-Vivi@users.noreply.github.com>
2024-04-07 12:09:49 -0400

    update gailbot_sample

--- gailbot_sample.py
+++ gailbot_sample.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 # @Author: Vivian Li
 # @Date:   2024-04-05 07:48:22
-# @Last Modified by:   Vivian Li
-# @Last Modified time: 2024-04-05 12:40:23
+# @Last Modified by:   Vivian Li 
+# @Last Modified time: 2024-04-07 10:23:18
 from gailbot import GailBot
 from gailbot import ProfileSetting
-from tqdm import tqdm
 
-INPUT = "./input/test2ab/"
+INPUT = "./input/hello2.wav"
 OUTPUT = "./output/"
+GOOGLE_API = (
+    "../input/google-api.json"  # TODO: need to change with existing path to google api
+)
 
 
 # basic usage using default whisperX engine and HiLabSuite setting
 def transcribe_with_default():
     gb = GailBot()
-    gb.add_source(source_path=INPUT, output_dir="../output/")
+    gb.add_source(source_path=INPUT, output_dir=OUTPUT)
     result = gb.transcribe()
     print(f"WhisperX default profile transcription result: {result}")
 
@@ -22,10 +24,7 @@ def transcribe_with_default():
 # transcription with google engine
 def transcribe_with_google():
     gb = GailBot()
-    google_engine_setting = {
-        "engine": "google",
-        "google_api_key": "../input/google-api.json",
-    }
+    google_engine_setting = {"engine": "google", "google_api_key": GOOGLE_API}
     google_engine_name = "google engine"
 
     google_profile_setting = ProfileSetting(
@@ -40,10 +39,10 @@ def transcribe_with_google():
         gb.add_engine(name=google_engine_name, setting=google_engine_setting)
 
     if not gb.is_profile(google_profile_name):
-        gb.create_profile(name=google_profile_name, data=google_profile_setting)
+        gb.create_profile(name=google_profile_name, setting=google_profile_setting)
 
     source_id = gb.add_source(INPUT, OUTPUT)
-    gb.apply_profile_to_source(source_name=source_id, profile_name=google_profile_name)
+    gb.apply_profile_to_source(source_id=source_id, profile_name=google_profile_name)
     google_transcription_result = gb.transcribe()
     print(f"Google transcription result: {google_transcription_result}")
 
@@ -70,10 +69,10 @@ def transcribe_with_whisper():
         gb.add_engine(name=whisper_engine_name, setting=whisper_engine_setting)
 
     if not gb.is_profile(whisper_profile_name):
-        gb.create_profile(name=whisper_profile_name, data=whisper_profile_setting)
+        gb.create_profile(name=whisper_profile_name, setting=whisper_profile_setting)
 
     source_id = gb.add_source(INPUT, OUTPUT)
-    gb.apply_profile_to_source(source_name=source_id, profile_name=whisper_profile_name)
+    gb.apply_profile_to_source(source_id=source_id, profile_name=whisper_profile_name)
     whisper_transcription_result = gb.transcribe()
     print(f"Whisper transcription result: {whisper_transcription_result}")
 
@@ -81,7 +80,6 @@ def transcribe_with_whisper():
 def transcribe_with_watson():
     gb = GailBot()
     WATSON_API_KEY = "MSgOPTS9CvbADe49nEg4wm8_gxeRuf4FGUmlHS9QqAw3"
-    WATSON_LANG_CUSTOM_ID = "41e54a38-2175-45f4-ac6a-1c11e42a2d54"
     WATSON_REGION = "dallas"
     WATSON_BASE_LANG_MODEL = "en-US_NarrowbandModel"
     watson_engine_name = "Watson Engine"
@@ -103,9 +101,9 @@ def transcribe_with_watson():
     if not gb.is_engine(watson_engine_name):
         gb.add_engine(name=watson_engine_name, setting=watson_engine_setting)
     if not gb.is_profile(watson_profile_name):
-        gb.create_profile(name=watson_profile_name, data=watson_profile)
+        gb.create_profile(name=watson_profile_name, setting=watson_profile)
     source_id = gb.add_source(INPUT, OUTPUT)
-    gb.apply_profile_to_source(source_name=source_id, profile_name=watson_profile_name)
+    gb.apply_profile_to_source(source_id=source_id, profile_name=watson_profile_name)
     watson_transcription_result = gb.transcribe()
     print(f"Watson transcription result: {watson_transcription_result}")
 
--- test_command.sh
+++ test_command.sh
@@ -2,9 +2,8 @@
 # @Author: Vivian Li 
 # @Date:   2024-04-05 08:41:53
 # @Last Modified by:   Vivian Li 
-# @Last Modified time: 2024-04-05 12:42:41
-conda create --name gb-test 
+# @Last Modified time: 2024-04-07 10:18:50
+conda create --name gb-test python==3.10
 conda activate gb-test
-conda install pip
-python -m pip install --extra-index-url https://test.pypi.org/simple/  GailBot==0.0.2a13
+python -m pip install --extra-index-url https://test.pypi.org/simple/  GailBot==0.0.2a16
 pip install git+https://github.com/m-bain/whisperx.git
